fbbff63424038330ecfe0dac002d5d5045f19f4e
Jacek Ostapczuk <jacekostapczuk86@gmail.com>
2023-01-20 09:22:07 +0100

    22.3.2  Client development

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
@@ -16,6 +18,11 @@ public class CreatedTrelloCard {
 
     @JsonProperty("shortUrl")
     private String shortUrl;
+
+
+    @JsonProperty("badges")
+    private TrelloBadgesDto badges;
+
 
 
 }
